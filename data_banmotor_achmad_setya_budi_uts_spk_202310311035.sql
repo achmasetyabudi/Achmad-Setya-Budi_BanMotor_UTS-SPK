c97a01c12d1840889fbae2b49135f38ccf8bbfda
achmasetyabudi <117165308+achmasetyabudi@users.noreply.github.com>
2023-10-31 10:50:11 +0700

    Add files via upload

---
+++ data_banmotor_achmad_setya_budi_uts_spk_202310311035.sql
@@ -0,0 +1,11 @@
+INSERT INTO "07TPLP015".data_banmotor_achmad_setya_budi_uts_spk ("No",merek,jenis,ukuran,tekanan,harga,altertantif,"Nilai Akhir","Rank") VALUES
+	 (1,'IRC','Tubeless','90/90-17','34 PSI','Rp. 100.000',3.385101,0.116931945,4),
+	 (2,'FDR','No Tubeless','70/90-17','32 PSI','Rp. 150.000',1.2400041,0.042833604,10),
+	 (3,'Swallow','No Tubeless','120/90-17','36 PSI','Rp. 200.000',1.7747136,0.061304137,7),
+	 (4,'Maxxis','Tubeless','100/90-17','32 PSI','Rp. 250.000',1.6478654,0.16115953,1),
+	 (5,'Dunlop','Tubeless','150/90-17','36 PSI','Rp. 300.000',4.66546,0.16115953,2),
+	 (6,'Pirelli','Tubeless','120/90-17','34 PSI','Rp. 350.000',3.5821612,0.12373901,6),
+	 (7,'Bridgestone','Tubeless','150/90-17','36 PSI','Rp. 400.000',4.66546,0.16115953,3),
+	 (8,'Michelin','No Tubeless','100/90-17','32 PSI','Rp. 450.000',2.2286708,0.07698524,9),
+	 (9,'Metzeler','No Tubeless','150/90-17','36 PSI','Rp. 500.000',3.2126324,0.110974334,5),
+	 (10,'Mitas','Tubeless','120/70-17','34 PSI','Rp. 550.000',2.5472584,0.08799025,8);
